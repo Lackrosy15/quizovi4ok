dc9fb34b1f7b8323084c3e4f8d5820c1becb0cef
Viktoria <rodionova.viktoriya.99@bk.ru>
2025-07-10 17:54:03 +0300

    коммент по подгрузке json

--- src/main/java/testovichok/dao/DaoConstants.java
+++ src/main/java/testovichok/dao/DaoConstants.java
@@ -1,5 +1,5 @@
 package testovichok.dao;
 
 public class DaoConstants {
-    public static final String BASE_PATH = "C:\\Users\\LackRosy\\IdeaProjects\\quizovi4ok\\files";
+    public static final String BASE_PATH = "C:\\Users\\User\\IdeaProjects\\quizovi4ok\\files"; //указать свой путь к папке files для подгрузки данных
 }
